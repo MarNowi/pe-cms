46eff54be887886e15457ebd9b84e48a23a494e4
MarNowi <mn@peak-energy.gmbh>
2026-07-15 23:25:24 +0200

    feat(knowledge): combine copied energy lexicon data

---
+++ scripts/knowledge/data/energielexikon/index.mjs
@@ -0,0 +1,13 @@
+import { energielexikonChunk1 } from './chunk-1.mjs'
+import { energielexikonChunk2 } from './chunk-2.mjs'
+import { energielexikonChunk3 } from './chunk-3.mjs'
+import { energielexikonChunk4 } from './chunk-4.mjs'
+
+export const energielexikonSections = [
+  ...energielexikonChunk1,
+  ...energielexikonChunk2,
+  ...energielexikonChunk3,
+  ...energielexikonChunk4,
+]
+
+export default energielexikonSections
